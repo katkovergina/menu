169314ab70f397d44f852698e43183a2496411b7
katkovergina <51058477+katkovergina@users.noreply.github.com>
2021-06-15 18:03:41 +0300

    Add files via upload

---
+++ src/app/documents/child-doc-about/child-doc-about.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-child-doc-about',
+  templateUrl: './child-doc-about.component.html',
+  styleUrls: ['./child-doc-about.component.css']
+})
+export class ChildDocAboutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
