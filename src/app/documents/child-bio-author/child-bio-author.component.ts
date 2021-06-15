9e68eb68a57069050027573349cb7c0d423fca4d
katkovergina <51058477+katkovergina@users.noreply.github.com>
2021-06-15 18:03:03 +0300

    Add files via upload

---
+++ src/app/documents/child-bio-author/child-bio-author.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-child-bio-author',
+  templateUrl: './child-bio-author.component.html',
+  styleUrls: ['./child-bio-author.component.css']
+})
+export class ChildBioAuthorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
