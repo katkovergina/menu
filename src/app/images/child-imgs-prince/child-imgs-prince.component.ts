737b8b7572c4cbaf2bcdc3dc9c345d0822d2bf5f
katkovergina <51058477+katkovergina@users.noreply.github.com>
2021-06-15 18:10:27 +0300

    Add files via upload

---
+++ src/app/images/child-imgs-prince/child-imgs-prince.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-child-imgs-prince',
+  templateUrl: './child-imgs-prince.component.html',
+  styleUrls: ['./child-imgs-prince.component.css']
+})
+export class ChildImgsPrinceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
