0d0df0de570ab528e8d8524c49bc733e30fb95d3
katkovergina <51058477+katkovergina@users.noreply.github.com>
2021-06-15 18:11:03 +0300

    Add files via upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,21 @@ import { MenuComponent } from "./menu/menu.component";
 import { ImagesComponent } from "./images/images.component";
 import { DocumentsComponent } from "./documents/documents.component";
 import { ChartsComponent } from "./charts/charts.component";
+import { ChildDocAboutComponent } from "./documents/child-doc-about/child-doc-about.component";
+import {ChildBioAuthorComponent} from "./documents/child-bio-author/child-bio-author.component";
+import {ChildImgsPrinceComponent} from "./images/child-imgs-prince/child-imgs-prince.component";
+import {ChildImgsAntoineComponent} from "./images/child-imgs-antoine/child-imgs-antoine.component";
 
 const routes: Routes = [
   {
     path: '', component:MenuComponent, children: [
       {path: '', redirectTo: '/', pathMatch: 'full'},
       {path: 'docs', component: DocumentsComponent},
+      {path: 'child-doc-about', component: ChildDocAboutComponent},
+      {path: 'child-bio-author', component: ChildBioAuthorComponent},
       {path: 'imgs', component: ImagesComponent},
+      {path: 'child-imgs-prince', component: ChildImgsPrinceComponent},
+      {path: 'child-imgs-antoine', component: ChildImgsAntoineComponent},
       {path: 'charts', component: ChartsComponent}
     ]
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,10 @@ import { MenuComponent } from './menu/menu.component';
 import { DocumentsComponent } from './documents/documents.component';
 import { ImagesComponent } from './images/images.component';
 import { ChartsComponent } from './charts/charts.component';
+import { ChildDocAboutComponent } from './documents/child-doc-about/child-doc-about.component';
+import { ChildBioAuthorComponent } from './documents/child-bio-author/child-bio-author.component';
+import { ChildImgsPrinceComponent } from './images/child-imgs-prince/child-imgs-prince.component';
+import { ChildImgsAntoineComponent } from './images/child-imgs-antoine/child-imgs-antoine.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +20,11 @@ import { ChartsComponent } from './charts/charts.component';
     MenuComponent,
     DocumentsComponent,
     ImagesComponent,
-    ChartsComponent
+    ChartsComponent,
+    ChildDocAboutComponent,
+    ChildBioAuthorComponent,
+    ChildImgsPrinceComponent,
+    ChildImgsAntoineComponent
   ],
   imports: [
     BrowserModule,
